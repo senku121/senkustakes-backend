085f8ca1ec4f9d623a875797a1c8356bdc7010ac
ayush1622 <itsayush456@gmail.com>
2026-07-14 12:58:08 +0545

    Replace Nodemailer with Resend

--- services/mailService.js
+++ services/mailService.js
@@ -1,32 +1,14 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: Number(process.env.MAIL_PORT),
-    secure: process.env.MAIL_SECURE === "true",
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    },
+async function sendMail({ to, subject, html }) {
+    return await resend.emails.send({
+        from: process.env.MAIL_FROM,
+        to,
+        subject,
+        html
+    });
+}
 
-    connectionTimeout: 15000,
-    greetingTimeout: 15000,
-    socketTimeout: 15000,
-
-    logger: true,
-    debug: true
-});
-
-transporter.verify((err) => {
-    if (err) {
-        console.log("SMTP VERIFY ERROR");
-        console.log(err);
-    } else {
-        console.log("SMTP VERIFIED");
-    }
-});
-
-module.exports = {
-    sendMail: (options) => transporter.sendMail(options)
-};
+module.exports = { sendMail };
